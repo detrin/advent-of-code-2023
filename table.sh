635e23416dc3515a674dec14addc7f25504858a8
Daniel Herman <daniel.herman@protonmail.com>
2023-12-04 23:03:31 +0100

    Update README

--- table.sh
+++ table.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-go_d01_t1=$(./benchmark.sh -l go -d day01 -t task1 -r 500 2>/dev/null | tail -n1)
-go_d01_t2=$(./benchmark.sh -l go -d day01 -t task2 -r 500 2>/dev/null | tail -n1)
-go_d02_t1=$(./benchmark.sh -l go -d day02 -t task1 -r 500 2>/dev/null | tail -n1)
-go_d02_t2=$(./benchmark.sh -l go -d day02 -t task2 -r 500 2>/dev/null | tail -n1)
-go_d03_t1=$(./benchmark.sh -l go -d day03 -t task1 -r 500 2>/dev/null | tail -n1)
-go_d03_t2=$(./benchmark.sh -l go -d day03 -t task2 -r 500 2>/dev/null | tail -n1)
-go_d04_t1=$(./benchmark.sh -l go -d day04 -t task1 -r 500 2>/dev/null | tail -n1)
-go_d04_t2=$(./benchmark.sh -l go -d day04 -t task2 -r 500 2>/dev/null | tail -n1)
+runs1=1000
 
-rust_d01_t1=$(./benchmark.sh -l rust -d day01 -t task1 -r 500 2>/dev/null | tail -n1)
-rust_d01_t2=$(./benchmark.sh -l rust -d day01 -t task2 -r 500 2>/dev/null | tail -n1)
-rust_d02_t1=$(./benchmark.sh -l rust -d day02 -t task1 -r 500 2>/dev/null | tail -n1)
-rust_d02_t2=$(./benchmark.sh -l rust -d day02 -t task2 -r 500 2>/dev/null | tail -n1)
-rust_d03_t1=$(./benchmark.sh -l rust -d day03 -t task1 -r 500 2>/dev/null | tail -n1)
-rust_d03_t2=$(./benchmark.sh -l rust -d day03 -t task2 -r 500 2>/dev/null | tail -n1)
-rust_d04_t1=$(./benchmark.sh -l rust -d day04 -t task1 -r 500 2>/dev/null | tail -n1)
-rust_d04_t2=$(./benchmark.sh -l rust -d day04 -t task2 -r 500 2>/dev/null | tail -n1)
+go_d01_t1=$(./benchmark.sh -l go -d day01 -t task1 -r $runs1 2>/dev/null | tail -n1)
+go_d01_t2=$(./benchmark.sh -l go -d day01 -t task2 -r $runs1 2>/dev/null | tail -n1)
+go_d02_t1=$(./benchmark.sh -l go -d day02 -t task1 -r $runs1 2>/dev/null | tail -n1)
+go_d02_t2=$(./benchmark.sh -l go -d day02 -t task2 -r $runs1 2>/dev/null | tail -n1)
+go_d03_t1=$(./benchmark.sh -l go -d day03 -t task1 -r $runs1 2>/dev/null | tail -n1)
+go_d03_t2=$(./benchmark.sh -l go -d day03 -t task2 -r $runs1 2>/dev/null | tail -n1)
+go_d04_t1=$(./benchmark.sh -l go -d day04 -t task1 -r $runs1 2>/dev/null | tail -n1)
+go_d04_t2=$(./benchmark.sh -l go -d day04 -t task2 -r $runs1 2>/dev/null | tail -n1)
+
+rust_d01_t1=$(./benchmark.sh -l rust -d day01 -t task1 -r $runs1 2>/dev/null | tail -n1)
+rust_d01_t2=$(./benchmark.sh -l rust -d day01 -t task2 -r $runs1 2>/dev/null | tail -n1)
+rust_d02_t1=$(./benchmark.sh -l rust -d day02 -t task1 -r $runs1 2>/dev/null | tail -n1)
+rust_d02_t2=$(./benchmark.sh -l rust -d day02 -t task2 -r $runs1 2>/dev/null | tail -n1)
+rust_d03_t1=$(./benchmark.sh -l rust -d day03 -t task1 -r $runs1 2>/dev/null | tail -n1)
+rust_d03_t2=$(./benchmark.sh -l rust -d day03 -t task2 -r $runs1 2>/dev/null | tail -n1)
+rust_d04_t1=$(./benchmark.sh -l rust -d day04 -t task1 -r $runs1 2>/dev/null | tail -n1)
+rust_d04_t2=$(./benchmark.sh -l rust -d day04 -t task2 -r $runs1 2>/dev/null | tail -n1)
 
 echo "| Task       | Rust      | Go        |"
 echo "|------------|-----------|-----------|"
